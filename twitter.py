7cf1296a95f399771ef83a5235c2b5c46c8da7b8
payel237 <131609273+payel237@users.noreply.github.com>
2023-04-23 22:07:05 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,21 +1,21 @@
-#from pyspark.sql import *
-#from pyspark.sql.functions import *
-#from pyspark.sql.types import *
-#import numpy as np # linear algebra
-#import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-#from wordcloud import WordCloud, STOPWORDS
-"""
-def wordcloudbot():
-    """
-    """
-    df_twitter = spark.read.format("json").load("./data")
-    print(df_twitter)
-    print("hi..")
+# #from pyspark.sql import *
+# #from pyspark.sql.functions import *
+# #from pyspark.sql.types import *
+# #import numpy as np # linear algebra
+# #import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+# #import matplotlib.pyplot as plt
+# #import seaborn as sns
+# #from wordcloud import WordCloud, STOPWORDS
+# """
+# def wordcloudbot():
+#     """
+#     """
+#     df_twitter = spark.read.format("json").load("./data")
+#     print(df_twitter)
+#     print("hi..")
 
 
-if _name_ == "_main_":
-    wordcloudbot()
-"""
+# if _name_ == "_main_":
+#     wordcloudbot()
+# """
 print("Durgesh")
