31852e71f1a7f69d6498e3ec21ef07fb954ea14b
payel237 <131609273+payel237@users.noreply.github.com>
2023-04-23 23:13:11 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -67,11 +67,14 @@ def twitter_data_analyis():
     print("Twitter Data Emoji analysis")
     plt.figure(figsize=(20,10))
     print(emoji_frame.head(10).sort_values(by='count',ascending=False))
+    print("/n")
 
     #Invoking function to perform trend analysis on the data 
     wordcloud = twitter_data_trend_analysis(df_twitter_pandas)
-    print("Twitter data trend analysis")
+    print("Twitter data trend analysis report: Analysis report has been saved at Img folder as PNG")
+    wordcloud.to_file("img/trend_analysis_twitter_data.png")
     plt.axis('off')
     plt.imshow(wordcloud)
+    
 
 twitter_data_analyis()
