41b2b8e9270b0336175ab72b61e142d4e271262c
payel237 <131609273+payel237@users.noreply.github.com>
2023-04-23 19:03:52 +0200

    Add files via upload

---
+++ twitter.py
@@ -0,0 +1,18 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud, STOPWORDS
+
+def wordcloudbot():
+    """
+    """
+    df_twitter = spark.read.format("json").load("./data")
+    print(df_twitter)
+
+
+if _name_ == "_main_":
+    wordcloudbot()
