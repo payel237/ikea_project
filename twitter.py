6882a3df4397023ac8d8c3d527f52462907c6f6b
payel237 <131609273+payel237@users.noreply.github.com>
2023-04-23 22:10:08 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,6 +1,6 @@
-# #from pyspark.sql import *
-# #from pyspark.sql.functions import *
-# #from pyspark.sql.types import *
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 # #import numpy as np # linear algebra
 # #import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # #import matplotlib.pyplot as plt
