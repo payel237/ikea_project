bed674436fa1f81d1cc0485666069b8e97feb804
payel237 <131609273+payel237@users.noreply.github.com>
2023-04-24 20:59:19 +0200

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -15,19 +15,11 @@ unit_test_exit_code=$?
 
 # Check if unit tests passed
 if [ $unit_test_exit_code -eq 0 ]; then
-  echo "Unit tests passed."
-  
-  # Run code execution
-  echo "Running code execution..."
-  python3 /var/jenkins_home/workspace/ikea_assignment/twitter.py unit_test_exit_code=$?
+  python3 /var/jenkins_home/workspace/ikea_assignment/twitter.py 
   code_execution_exit_code=$?
-  
-  # Check if code execution completed successfully
   if [ $code_execution_exit_code -eq 0 ]; then
-    echo "Code execution completed successfully."
-  else
-    echo "Code execution failed with exit code $code_execution_exit_code."
+ 	:
   fi
 else
-  echo "Unit tests failed with exit code $unit_test_exit_code. Code execution skipped."
+  echo "Unit tests failed . Code execution skipped."
 fi
